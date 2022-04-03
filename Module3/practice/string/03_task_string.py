24fb93cd0dd9be9f68cbb17d295e6081ee34efe6
CyberLaoshi <84038382+CyberLaoshi@users.noreply.github.com>
2022-04-03 15:14:48 +0300

    Update 03_task_string.py

--- Module3/practice/string/03_task_string.py
+++ Module3/practice/string/03_task_string.py
@@ -1,3 +1,7 @@
 # Подсчитать количество букв «а» во введенной строке
 
 # TODO: your code here
+
+sent = input("Строка: ")
+print("Английская 'a' ", sent.count("a"))
+print("Русская 'a' ", sent.count("а"))
