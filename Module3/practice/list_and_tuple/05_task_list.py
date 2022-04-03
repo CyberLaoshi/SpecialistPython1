c7698ee7aa966789a42691a12704568b0970b490
CyberLaoshi <84038382+CyberLaoshi@users.noreply.github.com>
2022-04-03 17:03:05 +0300

    Update 05_task_list.py

--- Module3/practice/list_and_tuple/05_task_list.py
+++ Module3/practice/list_and_tuple/05_task_list.py
@@ -3,10 +3,17 @@
 # выровненного по правой стороне - слева короткие слова дополняем символами пробела
 
 # Исходные данные:
-fruits = ["яблоко", "банан", "киви", "арбуз"]
+fruits = ["банан", "киви", "арбуз", "авокадо", "гранат"]
 
 # TODO: your code here
-
+max_length = 0
+for fruit in fruits:
+    current_length = len(fruit)
+    if current_length > max_length:
+        max_length = current_length
+for count, value in enumerate(fruits, 1):
+    difference = max_length - len(value)
+    print(count, ". " + " " * difference, value)
 # Пример вывода:
 # 1. яблоко
 # 2.  банан
