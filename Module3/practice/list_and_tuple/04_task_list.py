058c9ebc5ec8bfe466940354616d7e88bf6b6e1f
CyberLaoshi <84038382+CyberLaoshi@users.noreply.github.com>
2022-04-03 16:49:01 +0300

    Update 04_task_list.py

--- Module3/practice/list_and_tuple/04_task_list.py
+++ Module3/practice/list_and_tuple/04_task_list.py
@@ -2,3 +2,10 @@
 # Вывести на экран сумму всех положительных элементов.
 
 # TODO: your code here
+list_numbers = [20, 5, 1, -4, 2, -3]
+total = 0
+
+for num in list_numbers:
+    total += num if num >= 0 else 0
+
+print(total)
