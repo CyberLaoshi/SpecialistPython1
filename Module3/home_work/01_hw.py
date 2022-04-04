e68d94b592488fca11fcaec1a49461b51038719b
CyberLaoshi <84038382+CyberLaoshi@users.noreply.github.com>
2022-04-04 15:39:56 +0300

    Update 01_hw.py

--- Module3/home_work/01_hw.py
+++ Module3/home_work/01_hw.py
@@ -4,6 +4,12 @@
 names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
 
 # TODO: your code here
-
+names_new = ""
+for i, name in enumerate(names, 1):
+    if i != len(names):
+        names_new = names_new + name + ", "
+    else:
+        names_new = names_new + name
+print(names_new)
 # Пример вывода:
 # Иван, Ирина, Вячеслав, Василий, Петр
