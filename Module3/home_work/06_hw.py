6fbb057f9521944dc1697527d7b50fe98d174fc5
CyberLaoshi <84038382+CyberLaoshi@users.noreply.github.com>
2022-04-09 18:31:28 +0300

    Update 06_hw.py

--- Module3/home_work/06_hw.py
+++ Module3/home_work/06_hw.py
@@ -30,16 +30,60 @@ items = [
         "brand": "puma",
         "price": 1700
     },
+    {
+        "name": "Кепка",
+        "brand": "reebok",
+        "price": 1000
+    },
 ]
 # Найдите:
 print("Товары на складе представлены брэндами: ")
 
 # TODO: your code here
+brand_list = []
+for item in items:
+    if item["brand"] not in brand_list:
+        brand_list.append(item["brand"])
+print(", ".join(brand_list))
 
 print("На складе больше всего товаров брэнда(ов): ")
 
 # TODO: your code here
 
+# Вариант 1
+# done = False
+# brand_quantity_list = []
+# for item in items:
+#     done = False
+#     for thing in brand_quantity_list:
+#         if item["brand"] == thing["brand"]:
+#             thing["quantity"] += 1
+#             done = True
+#     if done == False:
+#             brand_quantity_list.append(dict(brand=item["brand"], quantity = 1))
+# brand_quantity_list.sort(key=lambda x: x.get('quantity'), reverse=True)
+# print(brand_quantity_list[0]["brand"])
+
+# Вариант 2
+# brand_checked_list = []
+# brand_quantity_dic = {}
+# for item in items:
+#     if item["brand"] in brand_checked_list:
+#         brand_quantity_dic[item["brand"]] += 1
+#     else:
+#         brand_checked_list.append(item["brand"])
+#         brand_quantity_dic[item["brand"]] = 1
+# print(max(brand_quantity_dic, key = brand_quantity_dic.get))
+
+# Вариант 3
+from collections import Counter
+cnt = Counter()
+for item in items:
+    cnt[item["brand"]] += 1
+print(max(cnt))
+
 print("На складе самый дорогой товар брэнда(ов): ")
 
 # TODO: your code here
+items.sort(key=lambda x: x.get("price"), reverse=True)
+print(items[0]["brand"])
