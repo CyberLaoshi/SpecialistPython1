bdf8d4e671f186a005a891857dfe29e5d4439563
CyberLaoshi <84038382+CyberLaoshi@users.noreply.github.com>
2022-04-09 17:50:23 +0300

    Update 03_hw.py

--- Module3/home_work/03_hw.py
+++ Module3/home_work/03_hw.py
@@ -2,3 +2,15 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+random_num_list = [-100, -90, -80, -40, 30, 20, 50, 70, 99, 100]
+# Вариант 1
+# total = 0
+# for num in random_num_list:
+#     if num > 0 and num % 2 == 0:
+#         total += num
+# print(total)
+
+# Вариант 2
+filtered = filter(lambda num: num > 0 and num % 2 == 0, random_num_list)
+print(sum(filtered))
+
