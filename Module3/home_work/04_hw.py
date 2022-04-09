25d77410cc3c6351f9933f794622916df0bba9cd
CyberLaoshi <84038382+CyberLaoshi@users.noreply.github.com>
2022-04-09 17:52:38 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,20 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+import math
+random_list_num = [2, -5, 8, 9, -25, 25, 4, 3, 100, -110, -100, 121, 225]
+new_list_num = []
+
+# Вариант 1
+# for num in random_list_num:
+#     square_root = num ** .5
+#     if type(square_root) != complex:
+#         if square_root - int(square_root) == 0:
+#             new_list_num.append(int(square_root))
+# print(new_list_num)
+
+# Вариант 2
+filtered = filter(lambda num: num > 0 and math.isqrt(num) ** 2 == num, random_list_num)
+for item in filtered:
+    new_list_num.append(int(math.sqrt(item)))
+print(new_list_num)
