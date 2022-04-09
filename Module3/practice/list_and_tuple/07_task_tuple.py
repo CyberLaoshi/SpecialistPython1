8b73b33d2f73baa0f9faa5933f332af18304c6ae
CyberLaoshi <84038382+CyberLaoshi@users.noreply.github.com>
2022-04-09 18:37:29 +0300

    Update 07_task_tuple.py

--- Module3/practice/list_and_tuple/07_task_tuple.py
+++ Module3/practice/list_and_tuple/07_task_tuple.py
@@ -3,3 +3,11 @@
 tup = (2, 4, 6, -4, 12, 0, 5)
 
 # TODO: your code here
+# Вариант 1
+print(max(tup))
+
+# Вариант 2
+max_num = 0
+for num in tup:
+    max_num = num if num > max_num else max_num
+print(max_num)
