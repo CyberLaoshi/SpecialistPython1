08fbd41930e477cfb6740cccff5693209ba19cc1
CyberLaoshi <84038382+CyberLaoshi@users.noreply.github.com>
2022-04-10 12:23:58 +0300

    Update 01_task_dict.py

--- Module3/practice/dict/01_task_dict.py
+++ Module3/practice/dict/01_task_dict.py
@@ -1,10 +1,12 @@
 # Дан словарь, содержащий данные о товаре из магазина
 # "price" - цена товара в валюте "currency"
-# "count" - количествотовара в магазине
+# "count" - количество товара в магазине
 # Считая,что курс доллара равен dollar_rate
-# Вычислите стоимость всех товаров с названием "name" в долларах
+# Вычислите цену всех товаров с названием "name" в долларах
 
 item = {"name": "Кроссовки", "price": "7540.5", "currency": "rub", "count": "10"}
 dollar_rate = 74.12
 
 # TODO: your code here
+total = float(item["price"]) / dollar_rate * float(item["count"])
+print(total)
