71f2f915284d7adfe78a90f82feb2a153c62d1c8
CyberLaoshi <84038382+CyberLaoshi@users.noreply.github.com>
2022-04-10 12:34:07 +0300

    Update 02_task_dict.py

--- Module3/practice/dict/02_task_dict.py
+++ Module3/practice/dict/02_task_dict.py
@@ -3,7 +3,17 @@
 
 keys = ['name', 'surname', 'age', 'rate']
 values = ['Петр', 'Первый', 42, 1300]
-
+dictOne = {}
 # TODO: your code here
 # Нужно получить словарь:
 # {'name': 'Петр', 'surname': 'Первый', 'age': 42, 'rate': 1300}
+# Вариант 1
+# for i in range(len(keys)):
+#     key = keys[i]
+#     value = values[i]
+#     dictOne[key] = value
+# print(dictOne)
+
+# Вариант 2
+new_dict = {x: values[keys.index(x)] for x in keys}
+print(new_dict)
