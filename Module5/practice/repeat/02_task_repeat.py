e4d7da585f3c1ac7a94392eba64fb5114ea0d2a9
CyberLaoshi <84038382+CyberLaoshi@users.noreply.github.com>
2022-04-17 12:14:12 +0300

    Update 02_task_repeat.py

--- Module5/practice/repeat/02_task_repeat.py
+++ Module5/practice/repeat/02_task_repeat.py
@@ -4,4 +4,20 @@
 #  Пример палиндрома: 12321
 
 def palindrome(number):
-    pass
+    # Вариант 1
+    # num_reverse = 0
+    # num_modified = number
+    # while num_modified != 0:
+    #     num_reverse = num_reverse * 10 + num_modified % 10
+    #     num_modified = num_modified // 10
+    # return number == num_reverse
+
+    # Вариант 2
+    num_reverse = ""
+    num_string = str(number)
+    for index in range(len(num_string)):
+        num_reverse += num_string[-1]
+        num_string = num_string[:-1]
+    return number == int(num_reverse)
+
+print(palindrome(12321))
